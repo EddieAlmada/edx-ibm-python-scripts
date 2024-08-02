3c7c844d91f13dc4ad9d3c224f355853f8d5898c
EddieAlmada <almada00@hotmail.com>
2024-08-01 20:38:19 -0600

    [data_structure] Feature: Uploading exercises with no answers

---
+++ data_structure_exercises/python_data_structures.py
@@ -0,0 +1,65 @@
+# Exercise 1: Create a list by picking an odd-index items from the first list and even index items from the second
+# Given two lists, l1 and l2, write a program to create a third list l3 by picking an odd-index element from the list l1 and even index elements from the list l2.
+# Given:
+
+l1 = [3, 6, 9, 12, 15, 18, 21]
+l2 = [4, 8, 12, 16, 20, 24, 28]
+
+
+
+# Exercise 2: Remove and add item in a list
+# Write a program to remove the item present at index 4 and add it to the 2nd position and at the end of the list.
+# Given:
+
+list1 = [54, 44, 27, 79, 91, 41]
+
+
+# Exercise 3: Slice list into 3 equal chunks and reverse each chunk
+# Given:
+
+sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+
+
+# Exercise 4: Count the occurrence of each element from a list
+# Write a program to iterate a given list and count the occurrence of each element and create a dictionary to show the count of each element.
+# Given:
+
+sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+
+
+# Exercise 5: Create a Python set such that it shows the element from both lists in a pair
+# Given:
+
+first_list = [2, 3, 4, 5, 6, 7, 8]
+second_list = [4, 9, 16, 25, 36, 49, 64]
+
+# Exercise 6: Find the intersection (common) of two sets and remove those elements from the first set
+# Given:
+
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+
+# Exercise 7: Checks if one set is a subset or superset of another set. If found, delete all elements from that set
+# Given:
+
+first_set = {27, 43, 34}
+second_set = {34, 93, 22, 27, 43, 53, 48}
+
+
+# Exercise 8: Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list
+# Given:
+
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+
+# Exercise 9: Get all values from the dictionary and add them to a list but don’t add duplicates
+# Given:
+
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+
+
+# Exercise 10: Remove duplicates from a list and create a tuple and find the minimum and maximum number
+# Given:
+
+sample_list = [87, 45, 41, 65, 94, 41, 99, 94]
